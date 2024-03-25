8a4245080d9dbb362161de39a3f6603f6faac549
Anosy0 <lucas.theodore2004@gmail.com>
2024-03-25 15:19:56 +0100

    Ending background changing

--- WeatherApp2/androidApp/src/main/java/com/example/weatherapp/android/api/Background.kt
+++ WeatherApp2/androidApp/src/main/java/com/example/weatherapp/android/api/Background.kt
@@ -1,11 +1,10 @@
 package com.example.weatherapp.android.api
 
+import android.graphics.drawable.Drawable
+import android.widget.LinearLayout
+import com.example.weatherapp.android.R
 import org.json.JSONObject
 
 class Background {
-    fun setBackgroundImage(json: JSONObject){
-        when (json.get("icon")){
-            snow ->
-        }
-    }
+
 }
