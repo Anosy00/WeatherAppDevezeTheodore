6654ab43bb4054bdfb8b52d00c31d607b7fff76f
Anosy0 <lucas.theodore2004@gmail.com>
2024-03-25 14:57:55 +0100

    Create file

---
+++ WeatherApp2/androidApp/src/main/java/com/example/weatherapp/android/api/Background.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp.android.api
+
+import org.json.JSONObject
+
+class Background {
+    fun setBackgroundImage(json: JSONObject){
+        when (json.get("icon")){
+            snow ->
+        }
+    }
+}
