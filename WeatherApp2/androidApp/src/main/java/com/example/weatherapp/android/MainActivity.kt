ef5bf14da9405f02ec1399151a46e648d1406dbb
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2024-03-25 13:56:55 +0100

    fetch api

--- WeatherApp2/androidApp/src/main/java/com/example/weatherapp/android/MainActivity.kt
+++ WeatherApp2/androidApp/src/main/java/com/example/weatherapp/android/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.weatherapp.android
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.weatherapp.Greeting
+import com.example.weatherapp.WeatherAPI
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_page_layout)
+        val test : TextView = findViewById(R.id.test)
+        val api = WeatherAPI()
+        CoroutineScope(Dispatchers.Main).launch {
+
+            test.text = api.collectDataFromCity("Limoges")
+
+        }
+
+
     }
 }
---
+++ WeatherApp2/shared/src/commonMain/kotlin/com/example/weatherapp/WeatherAPI.kt
@@ -0,0 +1,24 @@
+package com.example.weatherapp
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsChannel
+import io.ktor.client.statement.bodyAsText
+import io.ktor.utils.io.ByteReadChannel
+
+class WeatherAPI {
+    private val apiKey = "HZEWWXN4GMPXSQV5GBCDPU3BA";
+    suspend fun collectDataFromCity(city : String): String {
+        val client = HttpClient()
+
+        return try {
+            val response: HttpResponse =
+                client.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/$city?key=$apiKey")
+            response.bodyAsText()
+        } finally {
+            client.close()
+        }
+    }
+}
